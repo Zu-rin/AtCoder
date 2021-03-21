2c05f310d71065a1fae59a1cadc15191e1e74286
Zu_rin <s165810t@gmail.com>
2021-03-21 10:53:51 +0900

    solved ABC196-A

---
+++ AtCoder_Beginner_Contest/ABC196A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <algorithm>
+#define rep(i, n) for(i = 0; i < (n); i++)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define INF 1 << 30
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pp;
+
+int main(void) {
+	ll n, x = 1;
+	cin >> n;
+	while (stoll(to_string(x) + to_string(x)) <= n)
+		x++;
+	cout << x - 1 << "\n";
+	return 0;
+}
