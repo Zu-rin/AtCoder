e0f662781bf2153d3da636af0a8d2e10a592f415
Zu-rin <s165810t@gmail.com>
2021-05-28 15:28:26 +0900

    solved Typical-22 (Auto-commit by _fileHandler.py)

---
+++ Typical_90/Typical-22.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <algorithm>
+#define rep(i, n) for(i = 0; i < (n); i++)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define INF 1 << 30
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pp;
+
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+int main(void) {
+	ll a, b, c, ans = 0;
+	cin >> a >> b >> c;
+	ll g = gcd(a, gcd(b, c));
+	ans += (a + b + c) / g - 3;
+	cout << ans << "\n";
+	return 0;
+}
