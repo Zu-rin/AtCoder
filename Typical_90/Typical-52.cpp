b05126c68e71b435eb158aa09e242c9373cb3235
Zu-rin <s165810t@gmail.com>
2021-06-13 15:43:37 +0900

    solved Typical-52 (Auto-commit by _fileHandler.py)

---
+++ Typical_90/Typical-52.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <algorithm>
+#define rep(i, n) for(i = 0; i < (n); i++)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define INF 1 << 30
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pp;
+
+int main(void) {
+	int num, i, j, k;
+	ll ans = 0;
+	cin >> num;
+	vector<vector<ll>> dp(num + 1, vector<ll>(6, 0));
+	dp[0][0] = 1;
+	rep(i, num) {
+		rep(j, 6) {
+			int x;
+			cin >> x;
+			rep(k, 6)
+				dp[i + 1][j] += dp[i][k] * x;
+			dp[i + 1][j] %= MOD;
+		}
+	}
+	rep(i, 6)
+		ans += dp[num][i];
+	cout << ans % MOD << "\n";
+	return 0;
+}
