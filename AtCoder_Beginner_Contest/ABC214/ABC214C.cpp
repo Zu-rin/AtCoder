3ee275bb36d9895d1a5f73b289096df6614bdeb0
Zu-rin <s165810t@gmail.com>
2021-08-19 13:52:45 +0900

    solved ABC214C (Auto-commit by _fileHandler.py)

---
+++ AtCoder_Beginner_Contest/ABC214/ABC214C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <algorithm>
+#define rep(i, n) for(i = 0; i < (n); i++)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define INF 1ll << 60
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pp;
+
+int main(void) {
+	int num, i, s = 0;
+	cin >> num;
+	vector<ll> a(num), ans(num);
+	rep(i, num)
+		cin >> a[i];
+	rep(i, num) {
+		cin >> ans[i];
+		if (ans[i] < ans[s])
+			s = i;
+	}
+	for (i = s; (i + 1) % num != s; i++)
+		chmin(ans[(i + 1) % num], ans[i] + a[i]);
+	rep(i, num)
+		cout << ans[i] << "\n";
+	return 0;
+}
