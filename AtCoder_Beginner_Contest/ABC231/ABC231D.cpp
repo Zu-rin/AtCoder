191251562709172773dc165ec3f843586c3adec0
Zu-rin <s165810t@gmail.com>
2022-02-07 14:18:45 +0900

    solved ABC231D (Auto-commit by _fileHandler.py)

--- AtCoder_Beginner_Contest/ABC231/ABC231D.cpp
+++ AtCoder_Beginner_Contest/ABC231/ABC231D.cpp
@@ -7,7 +7,7 @@
 #define rep(i, n) for(i = 0; i < (n); i++)
 #define chmax(x, y) x = max(x, y)
 #define chmin(x, y) x = min(x, y)
-#define MOD 1000000007
+#define MOD 998244353
 #define PI 3.14159265358979323846
 #define INF 1 << 30
 
