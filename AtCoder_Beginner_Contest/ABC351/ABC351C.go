1302f485c425646c3ac28d4c4e84591e7ea37644
Zu-rin <zu_rin@team-jj.com>
2025-12-11 22:27:41 +0900

    solved ABC351C.go

---
+++ AtCoder_Beginner_Contest/ABC351/ABC351C.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func (io *Io) NextInt64() int64 {
+	x, err := strconv.ParseInt(io.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n := io.NextInt()
+	d := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		d = append(d, io.NextInt())
+		for len(d) >= 2 && d[len(d)-1] == d[len(d)-2] {
+			d = append(d[:len(d)-2], d[len(d)-1]+1)
+		}
+	}
+	fmt.Println(len(d))
+}
