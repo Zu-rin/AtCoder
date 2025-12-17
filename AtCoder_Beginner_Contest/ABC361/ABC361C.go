40db379d75136c48b59e13020cdd99a067378d72
Zu-rin <zu_rin@team-jj.com>
2025-12-17 21:17:54 +0900

    solved ABC361C.go

---
+++ AtCoder_Beginner_Contest/ABC361/ABC361C.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func (io *Io) NextInt64() int64 {
+	x, err := strconv.ParseInt(io.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n, k := io.NextInt(), io.NextInt()
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = io.NextInt()
+	}
+	sort.Ints(a)
+	ans := a[n-1] - a[0]
+	for i := 0; i <= k; i++ {
+		ans = min(ans, a[n-1-k+i]-a[i])
+	}
+	fmt.Println(ans)
+}
