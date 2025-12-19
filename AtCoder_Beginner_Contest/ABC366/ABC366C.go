131d47f73675f064d089e6697cfab3c2130f059d
Zu-rin <zu_rin@team-jj.com>
2025-12-19 20:57:22 +0900

    solved ABC366C.go

---
+++ AtCoder_Beginner_Contest/ABC366/ABC366C.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	q := io.NextInt()
+	d := make(map[int]int)
+	for i := 0; i < q; i++ {
+		k := io.NextInt()
+		switch k {
+		case 1:
+			x := io.NextInt()
+			d[x]++
+		case 2:
+			x := io.NextInt()
+			if d[x] > 1 {
+				d[x]--
+			} else {
+				delete(d, x)
+			}
+		case 3:
+			fmt.Println(len(d))
+		}
+	}
+}
