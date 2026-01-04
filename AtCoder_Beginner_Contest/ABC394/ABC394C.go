3c5f4f87529e7fe01377d7dd2ec6b9215ed30c26
Zu-rin <zu_rin@team-jj.com>
2026-01-04 14:52:05 +0900

    solved ABC394C.go

---
+++ AtCoder_Beginner_Contest/ABC394/ABC394C.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	s := []byte(io.Text())
+	for i := len(s) - 2; i >= 0; i-- {
+		if s[i] == 'W' && s[i+1] == 'A' {
+			s[i], s[i+1] = 'A', 'C'
+		}
+	}
+	fmt.Println(string(s))
+}
