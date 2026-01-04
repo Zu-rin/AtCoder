4010c3c60141d25916ee92bf466fc14364ac4830
Zu-rin <zu_rin@team-jj.com>
2026-01-04 14:52:05 +0900

    solved ABC398C.go

---
+++ AtCoder_Beginner_Contest/ABC398/ABC398C.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n := io.NextInt()
+	d := map[int][]int{}
+	for i := 1; i <= n; i++ {
+		x := io.NextInt()
+		d[x] = append(d[x], i)
+	}
+	ans, ma := -1, 0
+	for x, c := range d {
+		if len(c) == 1 && x > ma {
+			ma = x
+			ans = c[0]
+		}
+	}
+	fmt.Println(ans)
+}
