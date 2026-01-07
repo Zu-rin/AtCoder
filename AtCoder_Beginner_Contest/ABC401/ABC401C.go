625b4ed981c28351ffd0d0863484f2c224282774
Zu-rin <zu_rin@team-jj.com>
2026-01-07 23:34:37 +0900

    solved ABC401C.go

---
+++ AtCoder_Beginner_Contest/ABC401/ABC401C.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n, k := io.NextInt(), io.NextInt()
+	mod := 1000000000
+	a := make([]int, n+1)
+	sum := k
+	for i := 0; i < min(k, n+1); i++ {
+		a[i] = 1
+	}
+	for i := k; i <= n; i++ {
+		a[i] = sum
+		sum -= a[i-k]
+		sum = (sum + a[i]) % mod
+	}
+	if a[n] < 0 {
+		a[n] += mod
+	}
+	fmt.Println(a[n])
+}
