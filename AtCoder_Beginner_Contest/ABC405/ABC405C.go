0a04c5d85b4d365adeed9cb0150de7434115f9d1
Zu-rin <zu_rin@team-jj.com>
2026-01-10 13:52:04 +0900

    solved ABC405C.go

---
+++ AtCoder_Beginner_Contest/ABC405/ABC405C.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n := io.NextInt()
+	a := make([]int, n)
+	sum := 0
+	for i := 0; i < n; i++ {
+		a[i] = io.NextInt()
+		sum += a[i]
+	}
+	ans := sum * sum
+	for i := 0; i < n; i++ {
+		ans -= a[i] * a[i]
+	}
+	fmt.Println(ans >> 1)
+}
