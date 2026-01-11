d7a8eebfadde9550f0ea18ba2b5f639be82ef0ff
Zu-rin <zu_rin@team-jj.com>
2026-01-11 22:01:19 +0900

    solved ABC407C.go

---
+++ AtCoder_Beginner_Contest/ABC407/ABC407C.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	s := io.Text()
+	b := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		x := (int(s[i]-'0') - b) % 10
+		if x < 0 {
+			x += 10
+		}
+		b += x
+	}
+	fmt.Println(b + len(s))
+}
