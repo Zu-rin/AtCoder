bdd0658884ee6cdb5f49d5c020ffd66507b8c7c8
Zu-rin <zu_rin@team-jj.com>
2026-01-12 12:57:35 +0900

    solved ABC408C.go

---
+++ AtCoder_Beginner_Contest/ABC408/ABC408C.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n, m := io.NextInt(), io.NextInt()
+	d := make([]int, n+2)
+	for i := 0; i < m; i++ {
+		l, r := io.NextInt(), io.NextInt()
+		d[l]++
+		d[r+1]--
+	}
+	ans := m
+	for i := 1; i <= n; i++ {
+		d[i] += d[i-1]
+		ans = min(ans, d[i])
+	}
+	fmt.Println(ans)
+}
