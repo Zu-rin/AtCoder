e6c64c74740e0cec0bddfc179ad7285b80e0273a
Zu-rin <zu_rin@team-jj.com>
2026-01-31 14:53:26 +0900

    solved ABC429C.go

---
+++ AtCoder_Beginner_Contest/ABC429/ABC429C.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n := io.NextInt()
+	d := make(map[int]int)
+	for i := 0; i < n; i++ {
+		x := io.NextInt()
+		d[x]++
+	}
+	ans := 0
+	for _, v := range d {
+		if v > 1 {
+			ans += (v * (v - 1) * (n - v)) >> 1
+		}
+	}
+	fmt.Println(ans)
+}
