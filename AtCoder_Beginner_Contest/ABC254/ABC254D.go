ca1532c5f1ab6d241a7882aedad260a34adbf4f1
Zu-rin <zu_rin@team-jj.com>
2026-02-26 22:12:20 +0900

    solved ABC254D.go

---
+++ AtCoder_Beginner_Contest/ABC254/ABC254D.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n := io.NextInt()
+	ans := 0
+	for i := 1; i <= n; i++ {
+		k := i
+		for a := 2; a*a <= k; a++ {
+			for k%(a*a) == 0 {
+				k /= a * a
+			}
+		}
+		for a := 1; k*a*a <= n; a++ {
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
