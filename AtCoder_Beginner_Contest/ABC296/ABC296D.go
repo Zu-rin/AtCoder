3bfc617cae82241a9a48ed9b527e436c3bdd5a28
Zu-rin <zu_rin@team-jj.com>
2026-03-04 23:35:56 +0900

    solved ABC296D.go

---
+++ AtCoder_Beginner_Contest/ABC296/ABC296D.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n, m := io.NextInt(), io.NextInt()
+	ans := 1 << 60
+	for a := 1; a <= n; a++ {
+		b := (m + a - 1) / a
+		if b <= n {
+			ans = min(ans, a*b)
+		}
+		if a > b {
+			break
+		}
+	}
+	if ans == 1<<60 {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(ans)
+	}
+}
