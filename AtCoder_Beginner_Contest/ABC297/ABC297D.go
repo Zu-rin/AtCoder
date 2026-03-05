2bdd9f8de705dfcebf38821d704ad617dbb3d98f
Zu-rin <zu_rin@team-jj.com>
2026-03-05 22:20:17 +0900

    solved ABC297D.go

---
+++ AtCoder_Beginner_Contest/ABC297/ABC297D.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	a, b := io.NextInt(), io.NextInt()
+	ans := 0
+	for a != b && min(a, b) > 0 {
+		a, b = min(a, b), max(a, b)
+		if b%a == 0 {
+			ans += b/a - 1
+			break
+		}
+		ans += b / a
+		b %= a
+	}
+	fmt.Println(ans)
+}
