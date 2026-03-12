f1c779bc6d8b9404dec65e13c5f97f7e5237a9f5
Zu-rin <zu_rin@team-jj.com>
2026-03-12 23:04:37 +0900

    solved ABC306D.go

---
+++ AtCoder_Beginner_Contest/ABC306/ABC306D.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n := io.NextInt()
+	a, b := 0, 0
+	for i := 0; i < n; i++ {
+		x, y := io.NextInt(), io.NextInt()
+		if x == 0 {
+			a = max(a, max(a+y, b+y))
+		} else {
+			b = max(b, a+y)
+		}
+	}
+	fmt.Println(max(a, b))
+}
