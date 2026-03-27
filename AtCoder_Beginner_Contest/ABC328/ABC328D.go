a920a6b0ef787d1284330c18cf7f39d7f62b66cc
Zu-rin <zu_rin@team-jj.com>
2026-03-27 22:20:42 +0900

    solved ABC328D.go

---
+++ AtCoder_Beginner_Contest/ABC328/ABC328D.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	s := io.Text()
+	ans := make([]byte, 0, len(s))
+	for i := range s {
+		if s[i] == 'C' && len(ans) >= 2 && string(ans[len(ans)-2:]) == "AB" {
+			ans = ans[:len(ans)-2]
+		} else {
+			ans = append(ans, s[i])
+		}
+	}
+	fmt.Println(string(ans))
+}
