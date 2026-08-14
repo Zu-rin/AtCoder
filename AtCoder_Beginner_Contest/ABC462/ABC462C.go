b35070508c317de958f034f513b1b11c447742fa
Zu-rin <zu_rin@team-jj.com>
2026-08-14 21:41:29 +0900

    solved ABC462C.go

---
+++ AtCoder_Beginner_Contest/ABC462/ABC462C.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Io struct {
+	In  *bufio.Scanner
+	Out *bufio.Writer
+}
+
+func NewIo(r io.Reader, w io.Writer) *Io {
+	const buf = 1 << 20
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, buf), buf)
+	return &Io{
+		In:  s,
+		Out: bufio.NewWriter(w),
+	}
+}
+
+func (io *Io) Text() string {
+	if !io.In.Scan() {
+		panic(io.In.Err())
+	}
+	return io.In.Text()
+}
+
+func (io *Io) NextInt() int {
+	x, err := strconv.Atoi(io.Text())
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func main() {
+	io := NewIo(os.Stdin, os.Stdout)
+	defer io.Out.Flush()
+	n, ans := io.NextInt(), 0
+	mi := 1 << 60
+	d := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		d[io.NextInt()] = io.NextInt()
+	}
+	for i := 1; i <= n; i++ {
+		if d[i] < mi {
+			mi = d[i]
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
